9cbe5dae7700a9069b55a97d527f9d579cbc23cd
lelexuexi <ios004@ios004demacbook-pro.local>
2016-09-13 13:10:36 +0800

    Initial Commit

---
+++ UUkk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UUkk
+//
+//  Created by ios004 on 16/9/13.
+//  Copyright © 2016年 zsb2c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UUkk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UUkk
+//
+//  Created by ios004 on 16/9/13.
+//  Copyright © 2016年 zsb2c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
